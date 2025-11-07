58d176ee8227e7b4b78097e3e5f7e28d36e0324e
BeshoyGorgi <beshoygorgi10@gamil.com>
2025-11-07 10:34:39 +0100

    Teste neue Änderungen

--- backend/db.js
+++ backend/db.js
@@ -1,21 +1,13 @@
-import mysql from "mysql2";
+import mysql from "mysql2/promise";
 
-const db = mysql.createConnection({
-  host: "localhost", //  http://localhost:3000/api/kinder
-  user: "Beshoy",
-  password: "Kirchengesang",
-  database: "faith_Points"
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
-// Promisify
-const dbPromise = db.promise();
-
-db.connect((err) => {
-  if (err) {
-    console.error("Fehler bei der Datenbankverbindung:", err.message);
-  } else {
-    console.log("Erfolgreich mit MySQL verbunden!");
-  }
-});
-
-export default dbPromise;
+export default pool;
