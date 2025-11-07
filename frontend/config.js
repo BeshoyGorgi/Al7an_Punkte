1d7edebc8a72e17712246ca30cd912bcf1db06aa
BeshoyGorgi <beshoygorgi10@gamil.com>
2025-11-07 23:52:28 +0100

    Test fertig

--- frontend/config.js
+++ frontend/config.js
@@ -1,4 +1,4 @@
 export const API_BASE_URL = "https://faith-points.onrender.com"; 
 
 //Wenn ich local arbeiten möchte http://localhost:3000
-// Und in .env auch bei DB_HOST --> localhost
+// Und in .env auch bei DB_HOST ---> localhost
